dc7fd5cbaacef9b3201b2b3eca89da54554a0579
AspIT-61752 <61752@edu.campusvejle.dk>
2024-05-27 15:00:27 +0200

    NotifyPropertyChangedFor now works, but I now have 5 new errors

---
+++ src/SoUs.Apps.Mobile.Android/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SoUs.Apps.Mobile.Android
+{
+    public partial class BaseViewModel : ObservableObject
+    {
+        public BaseViewModel()
+        {
+
+        }
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+        bool isBusy;
+
+        [ObservableProperty]
+        string title;
+
+        public bool IsNotBusy => !IsBusy;
+
+    }
+}
