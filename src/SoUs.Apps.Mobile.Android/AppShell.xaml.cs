4bb50b219e6c6c3082b217aa1956b45ee628ab9f
AspIT-61752 <61752@edu.campusvejle.dk>
2024-05-27 13:20:35 +0200

    Added .NET MAUI to the project

---
+++ src/SoUs.Apps.Mobile.Android/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SoUs.Apps.Mobile.Android
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/SoUs.Apps.Mobile.Android/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SoUs.Apps.Mobile.Android
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
