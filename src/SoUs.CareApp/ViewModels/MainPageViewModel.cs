8d61f894dd2bf94b2c021287d3e0b6090698a401
AspIT-61752 <61752@edu.campusvejle.dk>
2024-05-29 10:42:54 +0200

    moved MainPage to views, and refactored the code, where it’s needed

---
+++ src/SoUs.CareApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace SoUs.CareApp.ViewModels
+{
+    public partial class MainPageViewModel : BaseViewModel
+    {
+        #region Fields
+
+        private string welcomeMessage;
+
+        #endregion
+
+        #region Constructors
+
+        public MainPageViewModel()
+        {
+            WelcomeMessage = "Welcome to SoUs Care App!";
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string WelcomeMessage { get => welcomeMessage; set => SetProperty(ref welcomeMessage, value); }
+
+        #endregion
+    }
+}
