a7d4116347ea7caaad0ba7cf32af1c035ecce216
AspIT-61752 <61752@edu.campusvejle.dk>
2024-05-30 14:58:57 +0200

    IT WORKS!

--- src/SoUs.CareApp/ViewModels/MainPageViewModel.cs
+++ src/SoUs.CareApp/ViewModels/MainPageViewModel.cs
@@ -1,25 +1,37 @@
-﻿namespace SoUs.CareApp.ViewModels
+﻿using SoUs.Entity;
+using SoUs.Services;
+using System.Collections.ObjectModel;
+
+namespace SoUs.CareApp.ViewModels
 {
     public partial class MainPageViewModel : BaseViewModel
     {
         #region Fields
 
-        private string welcomeMessage;
+        private readonly ISosuService sosuService;
+
+        public ObservableCollection<Assignment> TodaysAssignments { get; } = new();
 
         #endregion
 
         #region Constructors
 
-        public MainPageViewModel()
+        public MainPageViewModel(ISosuService sosuService)
         {
-            WelcomeMessage = "Welcome to SoUs Care App!";
+            Title = "SoUs Care App";
+            this.sosuService = sosuService;
+            UpdateAssignments();
+        }
+
+        private async Task UpdateAssignments()
+        {
+            TodaysAssignments.Clear();
+            var assignments = await sosuService.GetAssignmentsForAsync(DateTime.Now, new Employee() { EmployeeId = 1 });
         }
 
         #endregion
 
         #region Properties
-
-        public string WelcomeMessage { get => welcomeMessage; set => SetProperty(ref welcomeMessage, value); }
 
         #endregion
     }
---
+++ src/SoUs.Services/ApiBase.cs
@@ -0,0 +1,59 @@
+﻿using SoUs.Entity;
+using System.Net.Http.Json;
+
+namespace SoUs.Services
+{
+    public abstract class ApiBase
+    {
+        #region Fields
+
+        protected Uri baseUri;
+
+        #endregion
+
+        #region Constructors
+
+        protected ApiBase(Uri apiUri)
+        {
+            baseUri = apiUri;
+        }
+
+        protected ApiBase(string uri) : this(new Uri(uri)) { }
+
+        protected virtual async Task<HttpResponseMessage> GetHttpAsync(string url)
+        {
+            // Build URI to ensure it is correct
+            Uri uri = new("");
+
+            // Call API
+            using HttpClient client = new HttpClient();
+            var response = await client.GetAsync(uri);
+
+            // Return response to caller
+            return response;
+        }
+
+        #endregion
+    }
+
+    public class SosuService : ApiBase, ISosuService
+    {
+        public SosuService(Uri apiUri) : base(apiUri) { }
+
+        public SosuService(string apiUri) : base(apiUri) { }
+
+        public async Task<List<Assignment>> GetAssignmentsForAsync(DateTime date, Employee employee)
+        {
+            string url = "";
+            var response = await GetHttpAsync(url);
+            var result = response.Content.ReadFromJsonAsAsyncEnumerable<Assignment>();
+            List<Assignment> assignments = await result.ToListAsync();
+            return assignments;
+        }
+    }
+
+    public interface ISosuService
+    {
+        Task<List<Assignment>> GetAssignmentsForAsync(DateTime date, Employee employee);
+    }
+}
