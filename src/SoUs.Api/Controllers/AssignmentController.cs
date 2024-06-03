474499ada13b864b4fa5ca1eb5dc0ec017d2a031
AspIT-61752 <61752@edu.campusvejle.dk>
2024-06-03 11:46:34 +0200

    AssignmentController should be done

---
+++ src/SoUs.Api/Controllers/AssignmentController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoUs.DataAccess;
+using SoUs.Entity;
+
+namespace SoUs.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssignmentController(IAssignmentRepository repo) : Controller
+    {
+        private readonly IAssignmentRepository repo = repo;
+
+        [HttpPost]
+        public void AddNewAssignment([FromQuery] Assignment assignment)
+        {
+            repo.Add(assignment);
+        }
+
+        [HttpDelete]
+        [Route(nameof(DeleteAssignment))]
+        public void DeleteAssignment([FromQuery] Assignment assignment)
+        {
+            repo.Delete(assignment);
+        }
+
+        [HttpDelete]
+        [Route(nameof(DeleteAssignmentBy))]
+        public void DeleteAssignmentBy([FromQuery] int id)
+        {
+            repo.DeleteBy(id);
+        }
+
+        [HttpGet]
+        [Route("GetByDate")]
+        public IEnumerable<Assignment> GetAssignmentBy([FromQuery] DateTime date)
+        {
+            return repo.GetAssignmentsOn(date);
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public ActionResult<Assignment> GetAssignmentBy([FromQuery] int id)
+        {
+            return repo.GetBy(id);
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IEnumerable<Assignment> GetAll()
+        {
+            return repo.GetAll();
+        }
+
+        [HttpGet]
+        [Route("GetAssignmentsFor")]
+        public IEnumerable<Assignment> GetAssignmentsFor([FromQuery] Employee employee)
+        {
+            return repo.GetAssignmentsFor(employee);
+        }
+
+        [HttpPut]
+        public void UpdateAssignmentBy([FromQuery] Assignment assignment)
+        {
+            repo.Update(assignment);
+        }
+    }
+}
